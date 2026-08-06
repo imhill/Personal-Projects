546357b1b9aa66c480f4fffa241e1a2178f37060
Ian Hill <129342357+imhill@users.noreply.github.com>
2026-08-05 21:05:46 -0400

    Moved to own repository

--- NYT-MiniMax/src/popup/popup.js
+++
@@ -1,8 +0,0 @@
-const sizeInput = document.getElementById("sizeInput");
-sizeInput.addEventListener("change",updateSize);
-
-/*function updateSize(){
-    chrome.runtime.sendMessage({data: (sizeInput.value)}, function(response){
-        console.log(response);
-    });
-}*/
